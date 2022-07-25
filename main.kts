f4801241b4352f7174ffa388122ad2e438bba1ba
BlondeNamazu <namazutitech15@gmail.com>
2022-07-25 10:38:04 +0900

    add toLowercaseOnly function

--- main.kts
+++ main.kts
@@ -1,3 +1,23 @@
+// TODO
+// 1. print out all the combination of anagrams
+// 2. find longest words that are anagrams
+// 3. find the set of anagrams containing the most words
+
+import java.io.File
+
+val dictionary = File("dictionary.txt").readLines()
+
+fun toLowercaseOnlyWord(originalWord: String): String {
+  var convertedWord = ""
+  originalWord.forEach { char ->
+    when {
+      'a' <= char && char <= 'z' -> convertedWord += char
+      'A' <= char && char <= 'Z' -> convertedWord += 'a' + (char - 'A')
+    }
+  }
+  return convertedWord
+}
+
 fun convertWordToMap(word: String): Map<Char, Int> {
   var charCountMap = mutableMapOf<Char, Int>()
   ('a'..'z').map{ char ->
@@ -8,4 +28,14 @@ fun convertWordToMap(word: String): Map<Char, Int> {
   }
   return charCountMap
 }
+
+val wordList = File("dictionary.txt")
+  .readLines()
+  .map { original -> toLowercaseOnlyWord(original) }
+
+println(toLowercaseOnlyWord("hoge").toString())
+println(toLowercaseOnlyWord("HOGE").toString())
+println(toLowercaseOnlyWord("h'oge").toString())
 println(convertWordToMap("hoge").toString())
+
+
